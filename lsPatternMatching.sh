ec34a8809d17587745b5bf02c09848f4f25c2adb
kenneth <kenneth12345645@gmail.com>
2025-08-17 17:47:39 +0800

    Add ls pattern matching

---
+++ lsPatternMatching.sh
@@ -0,0 +1,5 @@
+ls *[0-9]* # list all files that contain a number in the file name
+echo next command
+ls [A-Z]* # list all files that start with an uppercase letter
+echo next command
+ls ?????? # list all files that are six characters long
